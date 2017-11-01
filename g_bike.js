ea8e98c362d512441d549d3faef3995af4e10a8c
Unnur <uab1@hi.is>
2017-11-01 15:42:46 +0200

    Semantics

--- g_bike.js
+++ g_bike.js
@@ -14,11 +14,11 @@ Bike.prototype.update = function (du) {
     // Remember my previous position
     var prevX = this.cx;
     var prevY = this.cy;
-    
+
     // Compute my provisional new position (barring collisions)
     var nextX = prevX + this.xVel * du;
     var nextY = prevY + this.yVel * du;
-    
+
 	//Movement for the bike
 	//Left
     if (g_keys[this.GO_LEFT]) {
@@ -27,7 +27,7 @@ Bike.prototype.update = function (du) {
 			this.xVel = -5;
 			this.yVel = 0;
 		}
-    } 
+    }
 	//Right
 	else if (g_keys[this.GO_RIGHT]) {
 		if (this.xVel < 0) return;
@@ -35,7 +35,7 @@ Bike.prototype.update = function (du) {
 			this.xVel = 5;
 			this.yVel = 0;
 		}
-    } 
+    }
 	//Up
 	else if (g_keys[this.GO_UP]) {
 		if (this.yVel > 0) return;
@@ -43,7 +43,7 @@ Bike.prototype.update = function (du) {
 			this.yVel = -5;
 			this.xVel = 0;
 		}
-    } 
+    }
 	//Down
 	else if (g_keys[this.GO_DOWN]) {
 		if (this.yVel < 0) return;
@@ -52,13 +52,13 @@ Bike.prototype.update = function (du) {
 			this.xVel = 0;
 		}
     }
-    
+
     // Bounce off left and right edges
     if (nextX < 5 ||                 // left edge
         nextX > 395) {               // right edge
         this.xVel *= -1;
     }
-	
+
 	if (nextY < 5) {
 		this.yVel *= -1;
 	}
@@ -71,14 +71,14 @@ Bike.prototype.update = function (du) {
         this.reset();
     }
 
-	
+
     this.cx += this.xVel * du;
     this.cy += this.yVel * du;
 
 	//Núverandi hnit eru "pushuð" inní trailX og trailY.
 	//Ef annarhvor "Array-inn" verður lengri en trailLength:
 	//Framkvæmum við Array.shift();
-	
+
 	this.trailX.push(this.cx);
 	this.trailY.push(this.cy);
 	if (this.trailX.length > this.trailLength)
@@ -97,9 +97,9 @@ Bike.prototype.reset = function () {
 
 Bike.prototype.render = function (ctx) {
 	var c = this.color;
-    fillBox(ctx, this.cx, this.cy, 10,10, c);
-	for(var i = 0; i<this.trailX.length; i++){
-		fillBox(ctx, this.trailX[i] ,this.trailY[i] ,10 ,10 ,c);
+    fillBox(ctx, this.cx, this.cy, 10, 10, c);
+	for (var i = 0; i < this.trailX.length; i++) {
+		fillBox(ctx, this.trailX[i], this.trailY[i], 10, 10, c);
 	}
 	//Hér er framkvæmt fillBox á hvert gildi í trailX og trailY
 	//Út kemur hali
