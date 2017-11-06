ea3fd1c23be8c209dc02a520cd7ecfb174f3a6c9
peturinn <petur_t@hotmail.com>
2017-11-06 15:44:34 +0000

    Grid fyrir collision
    a

---
+++ Grid.js
@@ -0,0 +1,11 @@
+var GridHeight = 5;
+var GridWidth = 5;
+
+var grid = []; 
+for (var i = 0 ; i < 80; i++) {
+    grid[i] = [];
+    for (var j = 0; j < 80; j++) { 
+        grid[i][j] = 0;
+    }
+}
+
