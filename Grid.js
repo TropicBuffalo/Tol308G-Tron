165cef851509fb23b02360470b2c3efeca1f394d
TropicBuffalo <fannzio@gmail.com>
2017-11-08 13:14:07 +0000

    13:14

--- Grid.js
+++ Grid.js
@@ -4,13 +4,19 @@ var GridWidth = 5;
 //Makes the array for the grid.
 
 var grid = [];
+
 for (var i = 0 ; i < 80; i++) {
     grid[i] = [];
     for (var j = 0; j < 80; j++) {
         grid[i][j] = 0;
     }
 }
-
+for(var i = 0; i < 80; i++) {
+	grid[0][i] = 3;
+	grid[79][i] = 3;
+	grid[i][0] = 3;
+	grid[i][79] = 3;
+}
 
 //Creates the bricks.
 function createBricks() {
@@ -24,6 +30,9 @@ function createBricks() {
 //"Draws" the bricks.
 function drawBrick(x, y, type){
     switch (type) {
+			case 3:
+      			g_ctx.fillStyle = '#39FF14';
+      			break;			
     		case 2:
       			//Color does nothing, just there for the type.
       			g_ctx.fillStyle = 'red';
@@ -39,7 +48,7 @@ function drawBrick(x, y, type){
 
     if (type) {
         g_ctx.fillRect(x * GridWidth, y * GridHeight, GridWidth, GridHeight);
-		    g_ctx.strokeRect(x * GridWidth + 1, y * GridHeight + 1, GridWidth - 2, GridHeight - 2);
+		g_ctx.strokeRect(x * GridWidth + 1, y * GridHeight + 1, GridWidth - 2, GridHeight - 2);
     }
 }
 
@@ -51,20 +60,4 @@ function collide(nextX,nextY){
         return true;
     }
     return false;
-}
-
-//Can use this to create trail.
-function makeTrail(i, j, a) {
-  	if (a === 1) {
-  		grid[i][j] = 1;
-  	}
-  	if (a === 2) {
-  		grid[i][j] = 2;
-  	}
-  	if (a === 3) {
-  		grid[i][j] = 3;
-  	}
-  	if (a === 4) {
-  		grid[i][j] = 4;
-  	}
 }
--- g_bike.js
+++ g_bike.js
@@ -53,68 +53,68 @@ Bike.prototype.update = function (du) {
 		    }
     }
 
-    // Bounce off left and right edges
-    if (nextX < 5 ||                 // left edge
-        nextX > 394) { // right edge
-    		/*
-    		window.alert("Dead");
-    		*/
-    		this.reset();
-    		for (var i = 0 ; i < 80; i++) {
-    		    grid[i] = [];
-    			  for (var j = 0; j < 80; j++) {
-    				    grid[i][j] = 0;
+    //Collision with X axis
+    if (nextX < 5 || nextX > 394) {
+		this.reset();
+		//Reset the grid
+    	for (var i = 0 ; i < 80; i++) {
+    	    grid[i] = [];
+    		for (var j = 0; j < 80; j++) {
+				grid[i][j] = 0;
             }
         }
+		for(var i = 0; i < 80; i++) {
+			grid[0][i] = 3;
+			grid[79][i] = 3;
+			grid[i][0] = 3;
+			grid[i][79] = 3;
+		}
     }
-
-    // Bounce off top and bottom edges
-	  if (nextY < 5 ||
-        nextY > 394) {
-  			/*
-  			window.alert("Dead");
-  			*/
-  			this.reset();
-  			for (var i = 0; i < 80; i++) {
-  			    grid[i] = [];
-  			    for (var j = 0; j < 80; j++) {
-  				      grid[i][j] = 0;
+	
+	//Collision with Y axis
+	if (nextY < 5 || nextY > 394) {
+ 		this.reset();
+		//Reset the grid
+ 		for (var i = 0; i < 80; i++) {
+ 		    grid[i] = [];
+ 		    for (var j = 0; j < 80; j++) {
+			grid[i][j] = 0;
             }
         }
-
-  			/*
-  		    this.yVel *= -1;
-  			*/
-	  }
-/*
-	//Collision stuff.
-	if (collide(nextX,nextY)){
-		window.alert("collision");
+		for(var i = 0; i < 80; i++) {
+			grid[0][i] = 3;
+			grid[79][i] = 3;
+			grid[i][0] = 3;
+			grid[i][79] = 3;
+		}
 	}
-*/
+	//Collision with the trail, collide() is in Grid.js
+	if (collide(nextX,nextY)){
+    	this.reset;
+		//Reset the grid
+		for (var i = 0; i < 80; i++) {
+ 		    grid[i] = [];
+ 		    for (var j = 0; j < 80; j++) {
+			grid[i][j] = 0;
+            }
+        }
+		for(var i = 0; i < 80; i++) {
+			grid[0][i] = 3;
+			grid[79][i] = 3;
+			grid[i][0] = 3;
+			grid[i][79] = 3;
+		}
+    }
+	
+	//Paint the trails of the players
     var brickX = Math.floor (prevX / 5.00);
-	  var brickY = Math.floor (prevY / 5.00);
-	  if (this.player === 1) {
-		    grid[brickX][brickY] = 1;
-	  }
-
+	var brickY = Math.floor (prevY / 5.00);
+	if (this.player === 1) {
+	    grid[brickX][brickY] = 1;
+	}
     else if (this.player === 2) {
-		    grid[brickX][brickY] = 2;
-	  }
-
-
-	// if(trailPaint(prevX,prevY)){
-	// 	drawBrick(prevX,prevY,1);
-	// }
-
-    	//Collision stuff.
-    	if (collide(nextX,nextY)){
-    		window.alert("collision");
-    	}
-
-	  // if (trailPaint(prevX, prevY)) {
-		//     drawBrick(prevX, prevY, 1);
-	  // }
+	    grid[brickX][brickY] = 2;
+	}
 
     // Reset if we fall off the left or right edges
     // ...by more than some arbitrary `margin`
@@ -124,19 +124,6 @@ Bike.prototype.update = function (du) {
 
     this.cx += this.xVel;
     this.cy += this.yVel;
-
-  	//Núverandi hnit eru "pushuð" inní trailX og trailY.
-  	//Ef annarhvor "Array-inn" verður lengri en trailLength:
-  	//Framkvæmum við Array.shift();
-    /*
-  	this.trailX.push(this.cx);
-  	this.trailY.push(this.cy);
-
-  	if (this.trailX.length > this.trailLength)
-  		  this.trailX.shift();
-  	if (this.trailY.length > this.trailLength)
-  		  this.trailY.shift();
-	  */
 };
 
 Bike.prototype.reset = function () {
@@ -149,9 +136,6 @@ Bike.prototype.reset = function () {
         this.cx = 100;
         this.cy = 300;
     }
-
-    // this.cx = 10;
-    // this.cy = 10;
     this.xVel = 5;
     this.yVel = 0;
 };
@@ -159,29 +143,4 @@ Bike.prototype.reset = function () {
 Bike.prototype.render = function (ctx) {
   	var c = this.color;
     fillBox(ctx, this.cx, this.cy, 5, 5, c);
-/*
-	  for (var i = 0; i < this.trailX.length; i++) {
-		    fillBox(ctx, this.trailX[i], this.trailY[i], 5, 5, c);
-	  }
-*/
-  	//Hér er framkvæmt fillBox á hvert gildi í trailX og trailY
-  	//Út kemur hali
-};
-/*
-Bike.prototype.collidesWith = function (prevX, prevY,
-                                          nextX, nextY) {
-    var bikeEdge = this.cy;
-    // Check X coords
-    if ((nextY < bikeEdge && prevY >= bikeEdge) ||
-        (nextY > bikeEdge && prevY <= bikeEdge)) {
-        // Check Y coords
-        if (nextX + 5 >= this.cx &&
-            nextX - 5 <= this.cx) {
-            // It's a hit!
-            return true;
-        }
-    }
-    // It's a miss!
-    return false;
-};
-*/
+}
